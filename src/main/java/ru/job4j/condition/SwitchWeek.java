f53a28372b5a25c1e0fa46dc068cacb1e1766fe3
Kolbaskakapchenaya <81241405+Kolbaskakapchenaya@users.noreply.github.com>
2023-09-04 14:31:32 +0300

    4.6.1. Switch [#334070]

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -6,18 +6,25 @@ public class SwitchWeek {
         switch (day) {
             case 1 :
                 name = "Понедельник";
+                break;
             case 2 :
                 name = "Втроник";
+                break;
             case 3 :
                 name = "Среда";
+                break;
             case 4 :
                 name = "Четверг";
+                break;
             case 5 :
                 name = "Пятница";
+                break;
             case 6 :
                 name = "Суббота";
+                break;
             case 7 :
                 name = "Воскресенье";
+                break;
             default:
                 name = "Ошибка";
         }
