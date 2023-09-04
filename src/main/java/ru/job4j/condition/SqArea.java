7ea0dc2f55c0bbc248ebad5d75c1e03c61fd3c2c
Kolbaskakapchenaya <81241405+Kolbaskakapchenaya@users.noreply.github.com>
2023-09-04 14:17:18 +0300

    .

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -1,11 +1,4 @@
 package ru.job4j.condition;
-
-/*
-h = p / (2 * (k + 1));
-l = h * k;
-s = l * h;
- */
-
 public class SqArea {
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
