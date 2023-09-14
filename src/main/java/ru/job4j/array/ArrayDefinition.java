120a858d156496f97f46d205519820dcf8972f1e
Kolbaskakapchenaya <81241405+Kolbaskakapchenaya@users.noreply.github.com>
2023-09-14 12:13:17 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        names[0] = "Vladimir";
+        names[1] = "Mikhail";
+        names[2] = "Petr";
+        names[3] = "Dmitriy";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
     }
 }
